cff2276d5290f9a831c9d1daaeaa4477ac83039f
krishna sekhar <krishna@hyperx2.ai>
2026-03-10 16:45:36 +0530

    cors issue

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/database");
 const cookieParser = require("cookie-parser")
+const cors = require("cors");
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
